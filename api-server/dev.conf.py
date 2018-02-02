5aa6168432e09c7f4dd41f5fdb8b262307c64813
Vincent Ahrend <mail@vincentahrend.com>
2018-02-02 07:22:13 +0100

    Change default db URL

--- api-server/dev.conf.py
+++ api-server/dev.conf.py
@@ -1,7 +1,7 @@
 ADMIN_KEY = 'saltyBeaver'
 DEBUG = True
 SECRET_KEY = "server development secret key"
-SQLALCHEMY_DATABASE_URI = "postgresql://localhost/metawahl"
+SQLALCHEMY_DATABASE_URI = "postgresql:///metawahl"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_RECORD_QUERIES = False
