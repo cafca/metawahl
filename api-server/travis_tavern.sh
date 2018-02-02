a9ee1e4f6a69b9e40923dd71834ad6d4a4a5d181
Vincent Ahrend <mail@vincentahrend.com>
2018-02-02 10:14:55 +0100

    Let travis bootstrap db and start server

---
+++ api-server/travis_tavern.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Starts the Flask development server in the background and then runs pytest suite,
+# killing the Flask process afterwards. For this to work, Flask debug mode must be 
+# deactivated, or an orphan process will be left over.
+
+python main.py &
+FLASK_PID=$!
+sleep 5
+pytest
+kill -9 $FLASK_PID
