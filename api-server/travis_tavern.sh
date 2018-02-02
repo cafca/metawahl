f441a3ba05ed9ca9fe2e061521411c0736b67105
Vincent Ahrend <mail@vincentahrend.com>
2018-02-02 10:22:04 +0100

    Add env vars to travis script

--- api-server/travis_tavern.sh
+++ api-server/travis_tavern.sh
@@ -4,6 +4,9 @@
 # killing the Flask process afterwards. For this to work, Flask debug mode must be 
 # deactivated, or an orphan process will be left over.
 
+export METAWAHL_CONFIG=prod.conf.py
+export ADMIN_KEY=asdfyooo
+
 python main.py &
 FLASK_PID=$!
 sleep 5
