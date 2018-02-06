a7e1683d2aa5ebf5d8cf0cdc4083398d69420601
Vincent Ahrend <mail@vincentahrend.com>
2018-02-06 15:59:06 +0100

    Eslint and flow config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,17 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    "plugin:flowtype/recommended"
+  ],
   env: {
     browser: true
   },
   rules: {
     "eqeqeq": "off"
-  }
+  },
+  "plugins": [
+    "flowtype"
+  ]
 };
