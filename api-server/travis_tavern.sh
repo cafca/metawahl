4509580f1951d90a8e88d70a3f193d6fad35394f
Vincent Ahrend <mail@vincentahrend.com>
2018-02-14 19:44:33 +0100

    Let pytest fail if tavern fails

--- api-server/travis_tavern.sh
+++ api-server/travis_tavern.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Starts the Flask development server in the background and then runs pytest suite,
-# killing the Flask process afterwards. For this to work, Flask debug mode must be 
+# killing the Flask process afterwards. For this to work, Flask debug mode must be
 # deactivated, or an orphan process will be left over.
 
 export METAWAHL_CONFIG=test.conf.py
@@ -10,4 +10,7 @@ python main.py &
 FLASK_PID=$!
 sleep 5
 pytest
+TEST_RESULT=$?
 kill -9 $FLASK_PID
+
+exit $TEST_RESULT
