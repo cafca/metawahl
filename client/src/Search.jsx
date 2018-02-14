959911e2bf71df9e550604915bd191c7f82a3267
Vincent Ahrend <mail@vincentahrend.com>
2018-02-14 12:17:42 +0100

    Close search on blur

--- client/src/Search.jsx
+++ client/src/Search.jsx
@@ -215,7 +215,7 @@ class SearchComponent extends React.Component<SearchProps, SearchState> {
         <div className="ui icon input">
           <input className="prompt" type="text" placeholder="Alles ist möglich..."
             onChange={this.handleSearchChange} value={this.state.query}
-            style={{borderRadius: 4}}></input>
+            style={{borderRadius: 4}} onBlur={this.reset}></input>
           <i className="search icon"></i>
         </div>
         <div className={resultClassName} style={{fontSize: "1.2em"}}>
