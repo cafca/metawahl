9fb20651a9c929cfe31caae5e33d5505f6639fc2
Vincent Ahrend <mail@vincentahrend.com>
2018-02-14 18:58:01 +0100

    Add react-snap

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const rootElement = document.getElementById('root');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+
 registerServiceWorker();
