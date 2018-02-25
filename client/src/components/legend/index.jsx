43d93ad6342a70a844fdb4e26c66fc7f7852f9bb
Vincent Ahrend <mail@vincentahrend.com>
2018-02-25 19:35:29 +0100

    Fix legend table html

--- client/src/components/legend/index.jsx
+++ client/src/components/legend/index.jsx
@@ -8,6 +8,7 @@ import { COLOR_PALETTE } from '../../config/';
 import './Legend.css';
 
 const Legend = () => <Table collapsing compact className='legend'>
+  <Table.Body>
     <Table.Row>
       <Table.Cell style={{fontWeight: 'normal'}}>
         <div className='square' style={{backgroundColor: COLOR_PALETTE[2]}}>&nbsp;</div> Zustimmung
@@ -19,6 +20,7 @@ const Legend = () => <Table collapsing compact className='legend'>
         <div className='square' style={{backgroundColor: COLOR_PALETTE[0]}}>&nbsp;</div> Ablehnung
       </Table.Cell>
     </Table.Row>
-  </Table>;
+  </Table.Body>
+</Table>;
 
 export default Legend;
