d66b1cd46b97c68ec46d361186353851a8e22500
Vincent Ahrend <mail@vincentahrend.com>
2018-02-27 20:22:51 +0100

    Fix attribute

--- client/src/views/occasionList/index.jsx
+++ client/src/views/occasionList/index.jsx
@@ -79,7 +79,7 @@ export default class OccasionList extends Component<RouteProps, State> {
     return <Container>
       <SEO title='Metawahl: Alle Wahlen im Überblick' />
       <Grid stackable columns={2} padded relaxed className='occasionList'>
-        <Grid.Row doubling>
+        <Grid.Row>
           <Grid.Column width={4} className='headerCount2'>
             <div className='headerCountInner'><div>{occasionCount > 0 ? occasionCount : 43}</div> Wahlen</div>
           </Grid.Column>
