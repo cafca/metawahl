d48ada437c52ff203e69447c7613cd720d626bd2
Vincent Ahrend <mail@vincentahrend.com>
2018-09-25 19:09:02 +0200

    Hotfix 3

--- client/src/views/occasion/index.jsx
+++ client/src/views/occasion/index.jsx
@@ -290,7 +290,7 @@ export default class Occasion extends React.Component<RouteProps, State> {
             : this.state.occasion.preliminary
               ? 'Welche Politik wird voraussichtlich bei der ' + this.state.occasion.title + ' gewählt?'
               : 'Welche Politik wurde bei der ' + this.state.occasion.title + ' gewählt?'}
-          { this.props.displayMode !== 'quiz' &&
+          { this.props.displayMode !== 'quiz' && this.state.occasion != null &&
             <Header.Subheader>
               { this.state.occasion.preliminary
               ? "Die Grafik zeigt, welcher Stimmanteil laut Wahlprognosen an Parteien geht, die sich für die jeweiligen Thesen ausgesprochen haben"
