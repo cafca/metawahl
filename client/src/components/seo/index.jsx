fd8d9b88d245791a7eb1cfaf5b6401ea91f4a9e1
Vincent Ahrend <mail@vincentahrend.com>
2018-10-10 08:54:36 +0200

    Add fathom tracking on page views without full reload

--- client/src/components/seo/index.jsx
+++ client/src/components/seo/index.jsx
@@ -1,51 +1,75 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { SITE_ROOT } from '../../config/';
+import React from "react";
+import Helmet from "react-helmet";
+import { SITE_ROOT } from "../../config/";
+import { withRouter } from 'react-router'
 
 const locales = {
-  "de": "de_DE"
+  de: "de_DE"
+};
+
+class Meta extends React.Component {
+  componentWillReceiveProps(nextProps) {
+    const isNewPage = this.props.location.pathname !== nextProps.location.pathname
+    if (isNewPage && Object.keys(window).indexOf('fathom') >= 0) {
+      window.fathom('trackPageview')
+    }
+  }
+
+  render() {
+    const data = this.props;
+    const lang = data.lang || "de";
+    const title = data.title;
+    const description =
+      data.description ||
+      "Welche Politik haben wir gewählt, als wir Parteien unsere Stimme gegeben haben?";
+    const image = data.image !== undefined && `${SITE_ROOT}${data.image}`;
+    const canonical =
+      data.canonical !== undefined && `${SITE_ROOT}${data.canonical}`;
+    const type = data.type === undefined ? "article" : "website";
+    const width = data.image && (data.width || 1200);
+    const height = data.image && (data.height || 630);
+
+    return (
+      <Helmet>
+        <html lang={lang} />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        {canonical ? <link rel="canonical" href={canonical} /> : null}
+        {image ? <link rel="image_src" href={image} /> : null}
+        {image ? <meta itemprop="image" content={image} /> : null}
+
+        <meta property="og:site_name" content="Metawahl" />
+        <meta property="og:title" content={title} />
+        {description ? (
+          <meta property="og:description" content={description} />
+        ) : null}
+        {canonical ? <meta property="og:url" content={canonical} /> : null}
+        <meta property="og:locale" content={locales[lang]} />
+        <meta property="og:type" content={type} />
+        {image ? <meta property="og:image" content={image} /> : null}
+        {width ? <meta property="og:image:width" content={width} /> : null}
+        {height ? <meta property="og:image:height" content={height} /> : null}
+        {/* <meta property="fb:pages" content="..." /> */}
+
+        {/* change type of twitter if there is no image? */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        {description ? (
+          <meta name="twitter:description" content={description} />
+        ) : null}
+        {image ? <meta name="twitter:image" content={image} /> : null}
+        <meta name="twitter:site" content="@metawahl_de" />
+        {canonical ? (
+          <link
+            rel="alternate"
+            href={`${SITE_ROOT}${data.canonical}`}
+            hreflang={lang}
+          />
+        ) : null}
+        {/* { canonical ? <link rel="alternate" href={ `${SITE_ROOT}${alternatePathname}` } hreflang={ alternateLang } /> : null } */}
+      </Helmet>
+    );
+  }
 }
 
-const Meta = (data) => {
-  const lang = data.lang || "de"
-  const title = data.title
-  const description = data.description || 'Welche Politik haben wir gewählt, als wir Parteien unsere Stimme gegeben haben?'
-  const image = data.image !== undefined && `${SITE_ROOT}${data.image}`
-  const canonical = data.canonical !== undefined && `${SITE_ROOT}${data.canonical}`
-  const type = data.type === undefined ? "article" : "website"
-  const width = data.image && (data.width || 1200)
-  const height = data.image && (data.height || 630)
-
-  return (
-    <Helmet>
-      <html lang={ lang } />
-      <title>{ title }</title>
-      <meta name="description" content={ description } />
-      { canonical ? <link rel="canonical" href={ canonical } /> : null }
-      { image ? <link rel="image_src" href={ image } /> : null }
-      { image ? <meta itemprop="image" content={ image } /> : null }
-
-      <meta property="og:site_name" content="Metawahl" />
-      <meta property="og:title" content={ title } />
-      { description ? <meta property="og:description" content={ description } /> : null }
-      { canonical ? <meta property="og:url" content={ canonical } /> : null }
-      <meta property="og:locale" content={ locales[lang] } />
-      <meta property="og:type" content={ type } />
-      { image ? <meta property="og:image" content={ image } /> : null }
-      { width ? <meta property="og:image:width" content={ width } /> : null }
-      { height ? <meta property="og:image:height" content={ height } /> : null }
-      {/* <meta property="fb:pages" content="..." /> */}
-
-      {/* change type of twitter if there is no image? */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={ title } />
-      { description ? <meta name="twitter:description" content={ description } /> : null }
-      { image ? <meta name="twitter:image" content={ image } /> : null }
-      <meta name="twitter:site" content="@metawahl_de" />
-      { canonical ? <link rel="alternate" href={ `${SITE_ROOT}${data.canonical}` } hreflang={ lang } /> : null }
-      {/* { canonical ? <link rel="alternate" href={ `${SITE_ROOT}${alternatePathname}` } hreflang={ alternateLang } /> : null } */}
-    </Helmet>
-  )
-}
-
-export default Meta
+export default withRouter(Meta);
