9dcb0a61b3cecb80f771e45e274bb54821b874b8
Vincent Ahrend <mail@vincentahrend.com>
2018-10-16 16:06:36 +0200

    Update Tavern tests

--- api/app/middleware/error.py
+++ api/app/middleware/error.py
@@ -21,11 +21,21 @@ def exceptions(e):
                  request.full_path,
                  tb)
 
-    return json_response(
-        {"error": "AAAHH! Serverfehler. Rute ist gezückt, Computer wird bestraft."},
-        status=500
-    )
+    try:
+        status = e.code
+    except AttributeError:
+        status = None
+
+    if status and status < 500:
+        return json_response({"error": e.name}, status = e.code)
+    else:
+        return json_response(
+            {
+                "error": "AAAHH! Serverfehler. Rute ist gezückt, Computer wird bestraft."
+            },
+            status = 500
+        )
 
 
 def page_not_found(e):
-    return json_response({"error": "Ressource not found"}, status=404)
+    return json_response({"error": "Ressource not found"}, status = 404)
