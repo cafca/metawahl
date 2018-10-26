198fbaa164d6933318ea263092090ca2fa3fb4d1
Vincent Ahrend <mail@vincentahrend.com>
2018-10-26 11:40:51 +0200

    Only purge cache if app had been loaded before

--- client/src/app/index.jsx
+++ client/src/app/index.jsx
@@ -121,9 +121,10 @@ class App extends Component<Props, State> {
         ? (a ^ ((Math.random() * 16) >> (a / 4))).toString(16)
         : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, uuid)
     }
-    if (loadFromCache("uuid") == null) saveToCache("uuid", uuid())
+    const uuidValue = loadFromCache("uuid")
+    if (uuidValue == null) saveToCache("uuid", uuid())
 
-    if (loadFromCache("api-version") !== API_VERSION) {
+    if (uuidValue != null && loadFromCache("api-version") !== API_VERSION) {
       deleteFromCache(["tags", "elections"])
     }
 
